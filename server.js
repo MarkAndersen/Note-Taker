524bade420d78736be9f65cce629d6c5a34951dd
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-06-12 14:00:15 -0400

    fixed push up

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 // dependencies and variables
 const express = require('express');
 const fs = require('fs');
-const uniqid = require('uniqid')
+const uniqid = require('uniqid');
 const path = require('path');
 const app = express();
 
-//port 
+//port
 const PORT = 3000;
-
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
@@ -16,53 +15,39 @@ app.use(express.static(path.join(__dirname, '/assets')));
 
 //requests handlers
 //display routes
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/notes.html')));
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/notes.html'))
+);
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
 
 //data routes
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', "utf8", (err, data)=> {
-        if (err) {
-            console.error(err);
-        } else {
-            res.send(data);
-        };
-        
-    })
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(data);
+    }
+  });
 });
 app.post('/api/notes', (req, res) => {
+  //not recognizing the below as a function??
   
-    //not recognizing the below as a function??
-       // savedNote.push(newNote);
-    fs.readFile('./db/db.json', "utf8", (err, data) => {
-        if (err) throw (err);
-        let savedNote = JSON.parse(data);
-        let newNote = req.body;
-        newNote['id'] = uniqid;
-        console.log(savedNote);
-    fs.writeFile("./db/db.json", JSON.stringify(data), err => {
-    if (err) {
-        console.error(err);
-    } res.send(newNote);
-});
-    // 
-    //     if (err) {
-    //         console.error(err);
-    //     } else{
-            
-          
-            
-    //     });
-
-    //     };
-        
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    //check to see if db is empty
+    let noteData = JSON.parse(data);
+    let newNote = req.body;
+    newNote['id'] = uniqid;
+    console.log(newNote);
+    noteData.push(newNote);
+    fs.writeFile('./db/db.json', JSON.stringify(noteData), (err) => {
+      if (err) throw err;
+      res.send(newNote);
     });
-    
-    
-    // generate unique ID's for return JSON
+  });
 });
 //port listener
 app.listen(PORT, () => {
-    console.log(`app listening at http://localhost:${PORT}`)
+  console.log(`app listening at http://localhost:${PORT}`);
 });
-
